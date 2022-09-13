a1ebc553671967257415d1ea7f16e79406b54f5e
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-09-13 18:23:02 +0600

    firebase issue

--- src/Auth/Firebase/Firebase.init.js
+++ src/Auth/Firebase/Firebase.init.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId:process.env.REACT_APP_measurementId
+  apiKey: "AIzaSyBk4evAfiaOIIhq8MrGHZKyIpzoQiYTc2U",
+  authDomain: "job-onboard.firebaseapp.com",
+  projectId: "job-onboard",
+  storageBucket: "job-onboard.appspot.com",
+  messagingSenderId: "595978804388",
+  appId: "1:595978804388:web:7d4f35e8d7b33d28579a31"
 };
 
 const app = initializeApp(firebaseConfig);
